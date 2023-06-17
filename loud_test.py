7135ec1820519c38035283613276277de8704e11
Sumaiya_Jannati <86371840+sumaiya200207@users.noreply.github.com>
2023-06-18 00:51:53 +0600

    again commit

--- loud_test.py
+++ loud_test.py
@@ -6,3 +6,11 @@ print("Sum " +s)
 text = "Python, a popular programming language"
 a = text.count("p")
 print(a)
+b = text.upper()
+print(b)
+
+listthis = ["apple", "pineapple", "goava", "mango"]
+listthis.append("banana")
+listthis.insert(2, "coconut")
+print(listthis)
+
