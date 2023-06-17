1c72b10a58fcac3746bf1864079805f9aca79222
Sumaiya_Jannati <86371840+sumaiya200207@users.noreply.github.com>
2023-06-18 01:27:33 +0600

    change commit

--- loud_test.py
+++ loud_test.py
@@ -10,7 +10,11 @@ b = text.upper()
 print(b)
 
 listthis = ["apple", "pineapple", "goava", "mango"]
+listthis1 = [1,3,5,7,8]
 listthis.append("banana")
 listthis.insert(2, "coconut")
+listthis.extend(listthis1)
+u = listthis1.copy()
 print(listthis)
+print(u)
 
