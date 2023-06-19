0a0ceab000964653977c49128b0b5a4ab919afba
Sumaiya_Jannati <86371840+sumaiya-HarleyQuinn@users.noreply.github.com>
2023-06-20 01:52:49 +0600

    final fibo

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,11 @@
-n = 10
+n = int(input("Enter range: "))
 a = 0
 b = 1
-print("0"+"1")
+print(0)
+print(1)
 for x in range(3, n+1):
     r = a + b
     print(r)
     a = b
     b = r
+print("Fibonacci done!")
