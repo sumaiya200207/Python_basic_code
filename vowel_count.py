7da584775bedcac8d3bbd3ab7e6550dc3db82e68
Sumaiya_Jannati <86371840+sumaiya-HarleyQuinn@users.noreply.github.com>
2023-06-21 23:57:53 +0600

    Final Vowel Commit

--- vowel_count.py
+++ vowel_count.py
@@ -5,5 +5,4 @@ count = 0
 for x in range(l):
     if sen[x] == 'a' or sen[x] == 'e' or sen[x] == 'i' or sen[x] == 'o' or sen[x] == 'u' or sen[x] == 'A' or sen[x] == 'E' or sen[x] == 'I' or sen[x] == 'O' or sen[x] == 'U':
         count = count + 1
-        print(sen[x]+" ")
-print(count)
+print("The number of the vowel is "+str(count))
