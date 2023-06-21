4969b8bee336370ec4661aa9953e6ea2964ca025
Sumaiya_Jannati <86371840+sumaiya-HarleyQuinn@users.noreply.github.com>
2023-06-21 23:37:43 +0600

    Vowel final commit

--- vowel_count.py
+++ vowel_count.py
@@ -3,7 +3,7 @@ sen = input("Enter a sentence: ")
 l = len(sen)
 count = 0
 for x in range(l):
-    if sen[x] == 'a' or sen[x] == 'e' or sen[x] == 'i' or sen[x] == 'o' or sen[x] == 'u':
+    if sen[x] == 'a' or sen[x] == 'e' or sen[x] == 'i' or sen[x] == 'o' or sen[x] == 'u' or sen[x] == 'A' or sen[x] == 'E' or sen[x] == 'I' or sen[x] == 'O' or sen[x] == 'U':
         count = count + 1
         print(sen[x]+" ")
 print(count)
